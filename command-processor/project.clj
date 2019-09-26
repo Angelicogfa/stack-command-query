3af6ba07e722b3aef0a794c26d3c087f6ffa9635
Guilherme Angelico <guilherme.angelico@s2it.com.br>
2019-09-26 17:16:38 -0300

    inclusão de log

--- command-processor/project.clj
+++ command-processor/project.clj
@@ -2,5 +2,6 @@
   :description "API para consumir eventos do kafka"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.apache.kafka/kafka-clients "1.1.0"]
-                 [cheshire "5.9.0"]]
+                 [cheshire "5.9.0"]
+                 [com.taoensso/timbre "4.10.0"]]
   :main command-processor.core)
